0c5681c94f042ddcdd7d5db54456796cd9d43335
E-Rick-M <erickmuirurinjuguna@gmail.com>
2025-05-12 23:55:18 +0300

    Worked on first demo draft of rag-qdrant

---
+++ main.py
@@ -0,0 +1,73 @@
+import requests
+from qdrant_client import QdrantClient
+from qdrant_client.models import Distance, VectorParams, PointStruct
+
+client=QdrantClient(url="http://localhost:6333")
+if not client.collection_exists(collection_name="demo"):
+    client.create_collection(
+        collection_name="demo",
+        vectors_config=VectorParams(size=1024, distance=Distance.DOT),
+    )
+
+""" dummy_data=[
+    "My name is Erick",
+    "I Like Watching Football",
+    "I like Learning new stuff",
+    "I like Cats",
+    "I like Dogs"
+] """
+
+def generate_response(prompt:str):
+    response=requests.post(
+        "http://localhost:11434/api/generate",
+        json={
+            "model": "gemma3:12b-it-qat",
+            "prompt":prompt,
+            "stream": False
+        }
+    )
+    return response.json()["response"]
+def main():
+    # for i, text in enumerate(dummy_data):
+    #     response=requests.post(
+    #         "http://localhost:11434/api/embed",
+    #         json={"model": "mxbai-embed-large", "input": text},
+    #     )
+    #     data=response.json()
+    #     embeddings=data["embeddings"][0]
+    #     client.upsert(
+    #         collection_name="demo",
+    #         wait=True,
+    #         points=[PointStruct(id=i,vector=embeddings,payload={"text": text})]
+    #     )
+    prompt=input("Enter a Prompt: ")
+    response=generate_response(prompt)
+    print(response)
+
+    # prompt=input("Enter a Prompt: ")
+    # adjusted_prompt=f"Represent this sentence for searching relevant passages: {prompt}"
+
+    # response=requests.post(
+    #     "http://localhost:11434/api/embed",
+    #     json={"model": "mxbai-embed-large", "input": adjusted_prompt},
+    # )
+    # data=response.json()
+    # embeddings=data["embeddings"][0]
+
+    # results=client.query_points(
+    #     collection_name="demo",
+    #         query=embeddings,
+    #         with_payload=True,
+    #         limit=2
+    # )
+
+    # for point in results.points:
+    #     print(point.payload["text"])
+
+
+
+
+
+
+if __name__=="__main__":
+    main()
